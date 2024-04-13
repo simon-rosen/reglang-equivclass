6a68d9c0178e1608eedb1ed4dccbb9842c11e596
Simon Rosén <sirosen@kth.se>
2024-04-13 11:13:55 +0200

    moved stuff to lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,7 @@
 module Main (main) where
 
-import           Data.List       (all, (\\))
-import           Data.Maybe
-import           Data.Set        (Set)
-import qualified Data.Set        as Set
 import           Lib
-import           Text.Regex.TDFA
 
 main :: IO ()
 main = someFunc
 
-alphabet = ["a", "b"]
-strings = concat $ iterate (\prev -> (++) <$> prev <*> alphabet) [""]
-regexToTest = "^(a*|ba)*$"
-
-eqCondition :: String -> String -> String -> Bool
-eqCondition x y z =
-  ((x <> z) =~ regexToTest :: Bool) == ((y <> z) =~ regexToTest :: Bool)
-
-eqClass :: String -> [String] -> [String]
-eqClass x stringsToTest = [y | y <- stringsToTest, all (== True) [eqCondition x y z | z <- stringsToTest]]
-
--- the Int is the number of strings to test on from Sigma^*
-partition :: Int -> [[String]]
-partition numStrings = go "" strings'
-  where
-    strings' = take numStrings strings
-    go first strings''
-      | strings'' == [] = []
-      | otherwise = let eqs = eqClass first strings'
-                        stringsLeft = strings'' \\ eqs
-                        first' = head stringsLeft
-                    in  eqs : (go first' stringsLeft)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,35 @@
 module Lib
-    ( someFunc
+    ( partition
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.List       (all, (\\))
+import           Data.Maybe
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
+import           Text.Regex.TDFA
+
+
+stringsOver :: [String] -> [String]
+stringsOver alphabet = concat $ iterate (\prev -> (++) <$> prev <*> alphabet) [""]
+
+eqCondition :: String -> String -> String -> String -> Bool
+eqCondition regexToTest x y z =
+  ((x <> z) =~ regexToTest :: Bool) == ((y <> z) =~ regexToTest :: Bool)
+
+eqClass :: String -> String -> [String] -> [String]
+eqClass regex x stringsToTest = [y | y <- stringsToTest, all (== True) [eqCondition regex x y z | z <- stringsToTest]]
+
+-- | partitions an alphabet (a subset of some size given) into
+-- groups that belong to the same equivalence class with respect
+-- to some regular language given as a regex.
+partition :: [String] -> String -> Int -> [[String]]
+partition alphabet regex numStrings = go "" strings
+  where
+    strings = take numStrings $ stringsOver alphabet
+    fullRegex = "^" <> regex <> "$"
+    go first stringsLeft
+      | null stringsLeft = []
+      | otherwise = let eqs = eqClass fullRegex first strings
+                        stringsLeft' = stringsLeft \\ eqs
+                        first' = head stringsLeft
+                    in  eqs : go first' stringsLeft'
